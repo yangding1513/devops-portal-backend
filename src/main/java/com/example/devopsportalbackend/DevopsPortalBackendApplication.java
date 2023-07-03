a4a15c2adbfa0f0310309e2b741a33f296df9b41
yinshui <3412772022@qq.com>
2023-07-03 09:04:31 +0800

    init

---
+++ src/main/java/com/example/devopsportalbackend/DevopsPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsPortalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsPortalBackendApplication.class, args);
+    }
+
+}
